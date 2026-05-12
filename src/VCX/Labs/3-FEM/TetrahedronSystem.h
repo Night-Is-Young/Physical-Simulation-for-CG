e335a050d323c02551f3801fe8f63b591a6e8c4d
Night-Is-Young <trackers@tutamail.com>
2026-05-12 14:46:11 +0800

    初步完成Tetrahedron.h头文件的编写，在四面体的基础上实现了四面体系统类型，包括基础参数。

---
+++ src/VCX/Labs/3-FEM/TetrahedronSystem.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Labs/3-FEM/Tetrahedron.h"
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace VCX::Labs::FEM {
+    struct TetrahedronSystem {
+        float Lx { 4.0f }, Ly { 4.0f }, Lz { 4.0f }; // Lengths of the system in x, y, z directions
+        int   nx { 16 }, ny { 16 }, nz { 16 };       // Number of subdivisions in x, y, z directions
+        glm::vec4 facecolor { 0.0f, 0.5f, 0.3f, 0.5f };
+        glm::vec4 edgecolor { 1.0f, 1.0f, 1.0f, 1.0f };
+
+        float _youngs_modulus { 20000.0f }; // Young's modulus for the material
+        float _rho { 400.0f };              // Density of the material
+        float _nu { 0.2f };                 // Poisson's ratio for the material
+        float _gravity { -0.05f };           // Gravitational acceleration
+
+        std::vector<Tetrahedron> _tetrahedra; // List of tetrahedra in the system
+        std::vector<Vertex>      _vertices;   // List of vertices in the system
+        std::vector<std::pair<int, int>> _edges; // List of edges in the system
+        std::vector<std::vector<int>>    _surfaceTriangles; // List of triangles in the system on the surface
+
+        void InitializeSystem();
+        void AdvanceTetrahedronSystem(const float dt);
+        int  _numperstep { 5 }; // Number of substeps for each time step
+        void SimulateTimeStep(const float dt) {
+            for (int i = 0; i < _numperstep; ++i) {
+                AdvanceTetrahedronSystem(dt / _numperstep);
+            }
+        }
+    };
+}
