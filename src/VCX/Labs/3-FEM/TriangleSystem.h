8ea25293ecf00ea249fd2523bab91de46fe20b88
Night-Is-Young <trackers@tutamail.com>
2026-05-13 12:02:37 +0800

    完成布料模拟的基本单元三角形类的编写。

--- src/VCX/Labs/3-FEM/TriangleSystem.h
+++ src/VCX/Labs/3-FEM/TriangleSystem.h
@@ -1,20 +1,20 @@
 #pragma once
 
-#include "Labs/3-FEM/Tetrahedron.h"
+#include "Labs/3-FEM/Triangle.h"
 #include <glm/glm.hpp>
 #include <vector>
 
 namespace VCX::Labs::FEM {
-    struct TetrahedronSystem {
-        float Lx { 8.0f }, Ly { 2.0f }, Lz { 2.0f }; // Lengths of the system in x, y, z directions
-        int   nx { 32 }, ny { 8 }, nz { 8 };       // Number of subdivisions in x, y, z directions
+    struct TriangleSystem {
+        float Lx { 2.0f }, Ly { 2.0f }; // Lengths of the system in x, y, z directions
+        int   nx { 32 }, ny { 32 };       // Number of subdivisions in x, y, z directions
         glm::vec4 facecolor { 0.0f, 0.5f, 0.3f, 0.5f };
         glm::vec4 edgecolor { 1.0f, 1.0f, 1.0f, 1.0f };
 
-        float _youngs_modulus { 20000.0f }; // Young's modulus for the material
-        float _rho { 400.0f };              // Density of the material
-        float _nu { 0.2f };                 // Poisson's ratio for the material
-        float _gravity { -0.05f };           // Gravitational acceleration
+        float _youngs_modulus { 50.0f }; // Young's modulus for the material
+        float _rho { 0.5f };              // Density of the material
+        float _nu { 0.3f };                 // Poisson's ratio for the material
+        float _gravity { -9.8f };           // Gravitational acceleration
 
         float _lambda { (_youngs_modulus / _rho) *_nu / ((1 + _nu) * (1 - 2 * _nu)) }; // Lamé's first parameter
         float _mu { (_youngs_modulus / _rho) / (2 * (1 + _nu)) };                              // Lamé's second parameter
@@ -24,13 +24,13 @@ namespace VCX::Labs::FEM {
         float _friction_ratio { 0.98f }; // Velocity reduction ratio for friction
         float _max_vel { 0.0f };    // Maximum velocity for clamping
 
-        std::vector<Tetrahedron> _tetrahedra; // List of tetrahedra in the system
+        std::vector<Triangle> _triangles; // List of triangles in the system
         std::vector<Vertex>      _vertices;   // List of vertices in the system
         std::vector<std::pair<int, int>> _edges; // List of edges in the system
         std::vector<std::vector<int>>    _surfaceTriangles; // List of triangles in the system on the surface
 
         int GetVertexIndex(int i, int j, int k) const {
-            return i * (ny + 1) * (nz + 1) + j * (nz + 1) + k;
+            return i * (ny + 1) + j;
         }
         glm::vec4 ColorMap(glm::vec3 v) {
             float ratio = std::min(glm::length(v) / (0.2f * std::sqrt(_youngs_modulus / _rho)), 1.0f);
